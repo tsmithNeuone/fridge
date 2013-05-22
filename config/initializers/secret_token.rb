cf2e6142644b4954e0e8d209130f255805fda598
Taylor Smith <tsmith@neuone.com>
2013-05-22 11:04:27 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fridge::Application.config.secret_token = '7cfed2bbd1acb56940fa6085cdbfe840366b39edc1a5620b996d901d5c110fa456f5a546818c89437ab21142ec9f9c75e7b25d342a3b39110a08b540decf3280'
